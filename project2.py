6826e496d0c69b29782dda18ca829c67caaf6d7f
px1208 <px12086@gmail.com>
2025-04-24 09:21:16 -0400

    1

--- project2.py
+++ project2.py
@@ -2,8 +2,16 @@ class duck():
    def __init__(self):
        self.name ="tree"
        self.title="water"
-
+       self.book={"Green Book"}
+       self.price="30"
 
    def Add(self):
     self.title = input("What book do you want to see")
+    print("You have added",self.title)
+    
+    self.title=input("enter the name of the book you would like to add: \n").upper
+    self.author=input("enter the author of the book you would like to added:\n").upper
+    self.price=float(input("enter the listing price of the book:\n"))
+    self.book[self.title] ={self.name:'tree','self.book':'Green book'}
+
     
